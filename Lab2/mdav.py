19e9d3cd427380624f379ee07f4f709eeed7b47b
musergi <sergi.mus@gmail.com>
2022-04-10 19:10:18 +0200

    Added loss and microaggregation

--- Lab2/mdav.py
+++ Lab2/mdav.py
@@ -1,14 +1,16 @@
 import argparse
-from re import A
 import pandas
 import random
 import json
+from collections import Counter
+
 
 def get_max_dist(df, record):
     dist = (df - record)
     sq_dist = dist * dist
     sq_dist['distance'] = sq_dist.sum(axis=1)
     return int(sq_dist.distance.idxmax())
+
 
 def get_closest(df, record_idx, n):
     record = df.loc[record_idx]
@@ -17,6 +19,29 @@ def get_closest(df, record_idx, n):
     sq_dist = dist * dist
     sq_dist['distance'] = sq_dist.sum(axis=1)
     return list(sq_dist.sort_values('distance').head(n).index)
+
+
+def mdav(df, k):
+    df = df.copy()
+    clusters = []
+    while len(df) >= 3 * k:
+        farthest = get_max_dist(df, df.mean())
+        other = get_max_dist(df, df.loc[farthest])
+        farthest_group = get_closest(df, farthest, k - 1) + [farthest]
+        other_group = get_closest(df, other, k - 1) + [other]
+        clusters.append(farthest_group)
+        clusters.append(other_group)
+        df = df.drop(farthest_group + other_group)
+    if len(df) >= 2 * k:
+        farthest = get_max_dist(df, df.mean())
+        farthest_group = get_closest(df, farthest, k - 1) + [farthest]
+        clusters.append(farthest_group)
+        df = df.drop(farthest_group)
+        clusters.append(list(df.index))
+    else:
+        clusters.append(list(df.index))
+    return clusters
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', required=True)
@@ -27,34 +52,39 @@ args = parser.parse_args()
 df = pandas.read_csv(args.file, index_col=0)
 
 df = (df - df.mean()) / df.std()
+dfc = df.copy()
 
 if args.display:
     df = df.drop(df.columns[2:], axis=1)
-    dfc = df.copy()
 
-clusters = []
+clusters = mdav(df, args.k)
 
-while len(df) >= 3 * args.k:
-    farthest = get_max_dist(df, df.mean())
-    other = get_max_dist(df, df.loc[farthest])
-    farthest_group = get_closest(df, farthest, args.k - 1) + [farthest]
-    other_group = get_closest(df, other, args.k - 1) + [other]
-    clusters.append(farthest_group)
-    clusters.append(other_group)
-    df = df.drop(farthest_group + other_group)
+dfc['cluster'] = -1
+for index, cluster in enumerate(clusters):
+    dfc.loc[cluster] = dfc.loc[cluster].mean().values
+    dfc.loc[cluster, 'cluster'] = index
+    
 
-if len(df) >= 2 * args.k:
-    farthest = get_max_dist(df, df.mean())
-    farthest_group = get_closest(df, farthest, args.k - 1) + [farthest]
-    clusters.append(farthest_group)
-    df = df.drop(farthest_group)
-    clusters.append(list(df.index))
-else:
-    clusters.append(list(df.index))
+dfc.drop('cluster', axis=1).to_csv(args.file + '.anon.csv')
+cluster_filepath = args.file + '.cluster.csv'
+dfc['cluster'].to_csv(cluster_filepath)
+
+diff = (df - dfc.drop('cluster', axis=1))
+sse = sum((diff * diff).sum())
+diff = df - df.mean()
+sst = sum((diff * diff).sum())
 
 print(f'Cluster count: {len(clusters)}')
-print(f'Cluster: \n{json.dumps(clusters,indent=2)}')
-print(f'Records in cluster: {list(map(len, clusters))}')
+print(f'Clusters: {cluster_filepath}')
+counter = Counter(list(map(len, clusters)))
+print(f'Records in cluster:')
+for size, count in counter.items():
+    s = '' if count == 1 else 's'
+    print(f'\t{count} cluster{s} with size {size}')
+
+print(f'SSE: {sse}')
+print(f'SST: {sst}')
+print(f'Loss: {sse / sst}')
 
 if args.display:
     import matplotlib.pyplot as plt
